6960ccba1c9f75296ae83d118a76ba1c6a663650
himzei <himzei@himzeiui-MacBookPro.local>
2021-01-25 12:29:29 +0900

    5.2

--- src/components/App.js
+++ src/components/App.js
@@ -12,16 +12,35 @@ function App() {
   }, 2000);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
-      if(user) {
-        setIsLoggedIn(true);
-        setUserObj(user);
+      if (user) {
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args),
+        });
       }
       setInit(true);
     });
   }, []);
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args),
+    });
+  };
   return (
     <>
-      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} /> : "Initializing ...."}
+      {init ? (
+        <AppRouter
+          refreshUser={refreshUser}
+          isLoggedIn={Boolean(userObj)}
+          userObj={userObj}
+        />
+      ) : (
+        "Initializing ...."
+      )}
       <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
     </>
   );
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,17 +1,48 @@
-import React from "react"; 
-import { authService } from "../fbase";
+import React, { useEffect, useState } from "react";
+import { authService, dbService } from "../fbase";
 import { useHistory } from "react-router-dom";
 
-
-export default () => {
-    const history = useHistory();
-    const onLogOutClick = () => {
-        authService.signOut();
-        history.push("/");
+export default ({ refreshUser, userObj }) => {
+  const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+      refreshUser();
     }
-    return ( 
-        <> 
-            <button onClick={onLogOutClick}>Logout</button>
-        </>
-    )
-}
+  };
+
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt", "desc")
+      .get();
+    console.log(nweets.docs.map((doc) => doc.data()));
+  };
+  useEffect(() => {
+    getMyNweets();
+  }, []);
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} type="text" placeholder="Display Name" value={newDisplayName} />
+        <input type="submit" value="Update profile" />
+      </form>
+      <button onClick={onLogOutClick}>Logout</button>
+    </>
+  );
+};
